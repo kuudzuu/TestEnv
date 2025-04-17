483b0f99c24ced68c83aca8eb2f01d0f5692108a
kuudzu <kuudzu.on@gmail.com>
2025-04-17 00:10:59 -0400

    New arena
    Created new area for more focused movement/physics testing

--- Scripts/Master.gd
+++ Scripts/Master.gd
@@ -1,7 +1,7 @@
 extends Node
 
 ## Scenes
-@onready var ARENA_SCENE = preload("res://Scenes/Terrain/Arena.tscn")
+@onready var ARENA_SCENE = preload("res://Scenes/Terrain/Arena2.tscn")
 @onready var PUCK_SCENE = preload("res://Scenes/Entities/Puck.tscn")
 
 # Called when the node enters the scene tree for the first time.
@@ -11,7 +11,7 @@ func _ready():
 	add_child(Arena)
 	add_child(Puck)
 	
-	Arena.initialize(Puck)
+	#Arena.initialize(Puck)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
